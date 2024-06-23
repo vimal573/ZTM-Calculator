f19072d231dda341b301a7d98393c69a95af53eb
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-06-23 09:08:44 +0530

    JS - Operator Functionality

--- script.js
+++ script.js
@@ -2,18 +2,46 @@ const calculatorDisplay = document.querySelector('h1');
 const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
 
+let firstValue = 0;
+let operatorValue = '';
+let awaitingNextValue = false;
+
 function sendNumberValue(number) {
-  //   If current display value is 0, replace it, if not add number
-  const displayValue = calculatorDisplay.textContent;
-  calculatorDisplay.textContent =
-    displayValue === '0' ? number : displayValue + number;
+  //  Replace current display value if first value is entered
+  if (awaitingNextValue) {
+    calculatorDisplay.textContent = number;
+    awaitingNextValue = false;
+  } else {
+    //   If current display value is 0, replace it, if not add number
+    const displayValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent =
+      displayValue === '0' ? number : displayValue + number;
+  }
 }
 
 function addDecimal() {
+  // If operator pressed, don't add decimal
+  if (awaitingNextValue) return;
+
   // If no decimal,add one
   if (!calculatorDisplay.textContent.includes('.')) {
     calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
   }
+}
+
+function useOperator(operator) {
+  const currentValue = Number(calculatorDisplay.textContent);
+  //   Assign fristValue if no value
+  if (!firstValue) {
+    firstValue = currentValue;
+  } else {
+    console.log('currentValue', currentValue);
+  }
+  //   Ready for next value , store operator
+  awaitingNextValue = true;
+  operatorValue = operator;
+  console.log('first Value', firstValue);
+  console.log('operator Value', operatorValue);
 }
 
 // Add Event Listeners for numbers, operators, decimal buttons
@@ -21,14 +49,17 @@ inputBtns.forEach(inputBtn => {
   if (inputBtn.classList.length === 0) {
     inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
   } else if (inputBtn.classList.contains('operator')) {
-    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+    inputBtn.addEventListener('click', () => useOperator(inputBtn.value));
   } else if (inputBtn.classList.contains('decimal')) {
     inputBtn.addEventListener('click', addDecimal);
   }
 });
 
-// Reset display
+// Reset all Values, display
 function resetAll() {
+  firstValue = 0;
+  operatorValue = '';
+  awaitingNextValue = false;
   calculatorDisplay.textContent = '0';
 }
 
